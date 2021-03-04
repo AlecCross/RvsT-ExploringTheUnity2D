04cedc8334ff5754e02f992a459e5fa90684d4a4
AlecCross <cross2472@gmail.com>
2021-03-04 12:57:09 +0200

    hw6-Добавил движение и стрельбу (Не работает прыжок и удаление пули)

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,34 +5,100 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     [SerializeField]
+    float playerLocalScale;
+    [SerializeField]
     float speed;
     [SerializeField]
     int lives;
     [SerializeField]
     float jumpForce;
-    new Rigidbody2D rigidbody;
+    Rigidbody2D playerRigidbody;
     Animation animator;
     SpriteRenderer sprite;
-    void Avake(){
+    [SerializeField]
+    bool isGrounded;
+    public GameObject bullet;
+    public GameObject bulSpawn;
+    void Avake()
+    {
+        jumpForce = 15.0f;
         speed = 3.0f;
         lives = 5;
-        jumpForce = 15.0f;
-        rigidbody = GetComponent<Rigidbody2D>();
+        
+        playerRigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animation>();
         sprite = GetComponentInChildren<SpriteRenderer>();
     }
+    void FixedUpdate(){
+        CheckGround();
+    }
     void Update()
     {
-        if(Input.GetButton("Horizontal")) Run();
-        if(Input.GetButton("Jump")) Jump();
+        if (Input.GetButton("Horizontal")) Run();
+        if (Input.GetButtonDown("Jump")) Jump();// && isGrounded
+        if (Input.GetButtonDown("Fire1")) Shoot();
+
+        // if (pl.transform.localScale.x == 1)
+        //     direction = new Vector3(1f, 0f, 0f);
+        // else direction = new Vector3(-1f, 0f, 0f);
+        // transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime * -25); if (act == false)
+        // {
+        //     lDirection = direction;
+        //     act = true;
+        // }
+        // transform.position = Vector3.MoveTowards(
+                            // transform.position, 
+                            // transform.position + lDirection, 
+                            // speed * Time.deltaTime);
+
     }
-    void Run(){
+    void Run()
+    {
         Vector3 direction = transform.right * Input.GetAxis("Horizontal");
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(
+                                        transform.position, 
+                                        transform.position + direction, 
+                                        speed * Time.deltaTime);
+        if (direction.x < 0f)
+            this.transform.localScale = new Vector3(-playerLocalScale,
+                                                     playerLocalScale, 
+                                                     playerLocalScale);    
+        else
+            this.transform.localScale = new Vector3(playerLocalScale, 
+                                                    playerLocalScale, 
+                                                    playerLocalScale);
         //sprite.flipX = direction.x < 0.0f;
     }
-
-    void Jump(){
-        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    public void Jump()
+    {
+        playerRigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+    void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.1f);
+        isGrounded = colliders.Length > 1;
+    }
+    void Shoot()
+    {
+        Vector3 position = bulSpawn.transform.position;
+        Instantiate(bullet, position, bullet.transform.rotation);
+    }
+    // void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     if (collision.gameObject.name == "FloorDown")
+    //     {
+    //         transform.position = rPoint.position;
+    //     }
+    //     if (collision.gameObject.name == "EndLevel")
+    //     {
+    //         transform.position = rPoint.position;
+    //     }
+    //     Destroy(this.gameObject);
+    // }
+    IEnumerator BulLife()
+    {
+        yield return new WaitForSeconds(4.0f); 
+        Destroy(this.gameObject);
+        StopCoroutine("BulLife");
     }
 }
---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    float speed = 10f;
+    Vector3 direction;
+    Vector3 lDirection;
+    GameObject player;
+    bool act = false;
+    public float playerLocalScale;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        //StartCoroutine(BulLife());
+        StartCoroutine("BulLife");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.transform.localScale.x==playerLocalScale)
+            direction = new Vector3(1f, 0f, 0f);
+        else    
+            direction = new Vector3(-1f, 0f, 0f);
+            transform.Rotate(new Vector3(0, 0, 45)  * Time.deltaTime*-25);
+            if(act==false){
+                lDirection = direction;
+                act=true;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, 
+                                                transform.position+lDirection,
+                                                speed * Time.deltaTime);
+    }
+    IEnumerable BulLife(){
+        print("StartCoroutine BulLife");
+        yield return new WaitForSeconds(4.0f);
+        Destroy(this.gameObject);
+        //StopCoroutine("BulLife");
+    }
+    void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.name == "Enemy"){}
+        print("destroy");
+        Destroy(this.gameObject);
+    }
+}
